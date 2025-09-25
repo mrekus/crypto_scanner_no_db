9809ccbd4e3bfb41cdb6a22e4355fe91e61db2b3
Marius Rekus <marius.rekus@gmail.com>
2025-09-25 15:53:53 +0300

    fixed alembic env file

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -18,8 +18,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from core.database import BaseModel
+target_metadata = BaseModel.metadata
 
+
+from utils.utils import import_all_models
+import_all_models()
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
@@ -38,7 +42,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    from core.database import database_url
+    url = database_url
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,6 +62,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    from core.database import database_url
+    url = database_url
+    config.set_main_option("sqlalchemy.url", url)
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,1 +1,12 @@
-# TODO: add any utility functions used
+import importlib
+import os
+from pathlib import Path
+
+
+def import_all_models():
+    project_root = Path(__file__).resolve().parent.parent
+    models_dir = project_root / "models"
+    for file in os.listdir(models_dir):
+        if file.endswith(".py") and file != "__init__.py":
+            module_name = f"models.{file[:-3]}"
+            importlib.import_module(module_name)
