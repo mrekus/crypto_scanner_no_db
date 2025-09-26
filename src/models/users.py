f6ea4bf9c0bfe4db82adf779cc9ecbc7fa5c8d18
Marius Rekus <marius.rekus@gmail.com>
2025-09-26 12:14:00 +0300

    added User model

--- src/models/users.py
+++ src/models/users.py
@@ -1,1 +1,20 @@
-# TODO: add user models, any other models required
+import datetime
+from enum import Enum
+
+from sqlalchemy import Column, Integer, String, Enum as SqlEnum, DateTime
+
+from core.database import BaseModel
+
+
+class ROLE(Enum):
+    USER = 0
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    last_login = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
+    role = Column(SqlEnum(ROLE, name='role'), nullable=False, default=ROLE.USER)
