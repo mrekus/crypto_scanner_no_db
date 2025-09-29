cf9ff487858f2bd0b6306a13621d111001a48226
Marius Rekus <marius.rekus@gmail.com>
2025-09-29 10:19:03 +0300

    fixed settings, pyproject.toml

--- src/conf/__init__.py
+++ src/conf/__init__.py
@@ -10,14 +10,28 @@ class Settings(BaseSettings):
     debug = True
     host = 'localhost'
     port = 8000
-    DATABASE = {
-        'user': 'crypto_scanner',
-        'password': 'crypto_scanner',
-        'host': 'localhost',
-        'port': 5432,
-        'database': 'crypto_scanner'
-    }
+    
+    database_user: str = ''
+    database_password: str = ''
+    database_host: str = 'localhost'
+    database_port: int = 5432
+    database_name: str = ''
+    
     # TODO: set session key before deploy
     SESSION_SECRET_KEY = ''
+    
+    @property
+    def DATABASE(self):
+        return {
+            'user': self.database_user,
+            'password': self.database_password,
+            'host': self.database_host,
+            'port': self.database_port,
+            'database': self.database_name,
+        }
+
+    class Config:
+        env_file = BASE_DIR / '.env'
+        env_file_encoding = 'utf-8'
 
 cfg = Settings()
